796c1fa4047092ddfae5894de658d32ed54bb236
Ludovico Fischer <livrerie@gmail.com>
2021-04-01 21:39:04 +0200

    Add another test case.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const isSvg = require('is-svg');
 
-const out = isSvg('<svg xmlns="http://www.w3.org/2000/svg"><filter id="filter"><feGaussianBlur stdDeviation="5" /></filter></svg>#filter')
-console.log(out); // should be false, but is true instead
+const out1 = isSvg('<svg xmlns="http://www.w3.org/2000/svg"><filter id="filter"><feGaussianBlur stdDeviation="5" /></filter></svg>#filter')
+console.log(out1); // should be false, but is true instead
+const out2 = isSvg('<svg xmlns="http://www.w3.org/2000/svg"><filter id="filter"><feGaussianBlur stdDeviation="5" /></filter></svg>filter')
+console.log(out2); // should be false, but is true instead
